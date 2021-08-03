1b6479208391fc2541db1d1857804574c3034e67
Dino Jose Almirall <almirall.dino@gmail.com>
2021-08-03 12:22:41 +0200

    Update header.php

--- website001/header.php
+++ website001/header.php
@@ -11,22 +11,34 @@
     <meta name="viewport" content= "width=device-width, initial-scale=1.0">
     <script src="./scripts/main.js" charset="utf-8"></script>
     <link rel="stylesheet" href="./css/master.css">
+    <link rel="stylesheet" href="./css/content.css">
     <link rel="icon" href="./img/favicon.ico">
-    <title></title>
+    <title>Site</title>
   </head>
   <body>
     <header>
+      <div class="logo">
+        <a href="./index.php">
+          <img src="./img/logo.png" alt="Site">
+          <h2>Site</h2>
+        </a>
+      </div>
+      <div class="navigation">
+
+      </div>
       <div class="logger">
-        <?php
-          if(isset($user)){
-            echo '
-              <form class="logout" action="./includes/logout.php" method="post">
-                <input class="loggerButton" type="submit" name="logout" value="LOGOUT">
-              </form>
-            ';
-          }else{
-            echo '<button class="loggerButton" onclick="login()">LOGIN</button>';
-          }
-         ?>
+        <div class="wrapper">
+          <?php
+            if(isset($user)){
+              echo '
+                <form class="logout" action="./includes/logout.php" method="post">
+                  <input class="loggerButton" type="submit" name="logout" value="LOGOUT">
+                </form>
+              ';
+            }else{
+              echo '<button class="loggerButton" onclick="loginOpen()">LOGIN</button>';
+            }
+           ?>
+        </div>
       </div>
     </header>
